480431c33642704d8ac851cfa69b00ad51bd79f5
David Masad <david.masad@gmail.com>
2013-04-05 23:41:13 -0400

    Begin project
    Started adding the file structure and skeleton.

---
+++ goalnet/model/task.py
@@ -0,0 +1,65 @@
+'''
+Created on Apr 5, 2013
+
+@author: dmasad
+'''
+
+class Task(object):
+    '''
+    A task that one or more agents will attempt to perform.
+    
+    For a task to succeed, a certain number of subtasks must be performed within
+    a specified timeframe. At the moment, this is completely abstract: subtasks
+    are interchangeable; the number of subtasks is the only important factor. 
+    
+    Attributes:
+        task_id: A unique identifier.
+        payoff: The payoff once the task is accomplished
+        subtasks: Number of subtasks to be undertaken for the task to succeed
+        timeframe: The number of clock ticks from the first subtask to the last
+            one for the task to succeed.
+        subtasks_executed: A list of ticks at which subtasks have been executed.
+        
+    '''
+
+
+    def __init__(self, task_id, payoff, subtasks, timeframe):
+        '''
+        Create a new task.
+        '''
+        self.task_id = task_id
+        self.payoff = payoff
+        self.subtasks = subtasks
+        self.timeframe = timeframe
+        self.subtasks_executed = []
+    
+    def execute_subtask(self, tick):
+        '''
+        Record the execution of a subtask at the specified tick
+        '''
+        
+        self.subtasks_executed.append(tick)
+    
+    def is_complete(self):
+        '''
+        Check if the task has been accomplished.
+        
+        Returns:
+            True if the task is complete,
+            False otherwise
+        '''
+        
+        if len(self.subtasks_executed) < self.subtasks:
+            return False
+        
+        start_time = max(self.subtasks_executed) - self.timeframe 
+        subtasks_allowed = [timestamp for timestamp in self.subtasks_executed
+                            if timestamp >= start_time]
+        if len(subtasks_allowed) >= self.subtasks:
+            return True
+        else:
+            return False
+
+        
+        
+        
